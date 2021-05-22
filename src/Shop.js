8b45117327bbdf6022a4556a1477af1cd7b8325b
Jenny Zonka <jennycade@gmail.com>
2021-05-22 14:54:27 -0700

    Style shop item cards

--- src/Shop.js
+++ src/Shop.js
@@ -3,11 +3,15 @@ import React from 'react';
 import shopData from './data/shop-data.json';
 import ShopItem from './ShopItem';
 
+import './Shop.css';
+
 const Shop = (props) => {
   return (
     <div className="Shop">
       <h1>Shop</h1>
-      { shopData.map(item => <ShopItem key={item.name} item={item} />) }
+      <div className="ShopItemDisplay">
+        { shopData.map(item => <ShopItem key={item.name} item={item} />) }
+      </div>
     </div>
   );
 };
--- src/ShopItem.js
+++ src/ShopItem.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { useState } from 'react';
 
+import './ShopItem.css'
+
 const ShopItem = (props) => {
-  const { item: name, imageURL, price, descripton } = props;
+  const { item: { name, imageURL, price, descripton } } = props;
 
-  // name
-  // image
-  // price
-  // description
-  // details page?
-
-  // quantity input
-  // add to cart button
   const [quantity, setQuantity] = useState(1);
   const [input, setInput] = useState('1');
 
@@ -27,9 +21,9 @@ const ShopItem = (props) => {
 
   return (
     <div className="ShopItem">
-      <title>{ props.item.name }</title>
-      <img src={ props.item.imageURL } alt={ props.item.name } />
-      <p>${ props.item.price }</p>
+      <img src={ imageURL } alt={ name } />
+      <p class="itemName">{ name }</p>
+      <p class="itemPrice">${ price }</p>
       <label>Quantity <input type="text" onChange={verifyQuantity} value={input} /></label>
       <button>Add { quantity } to cart</button>
 
